5974bb8762bb63903f96a2d0d68d39b4c6b4119e
Denali Balser <denali.balser167@gmail.com>
2019-09-26 14:15:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
   end
 
   def update
-    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self,breed, self.id)
   end
 
